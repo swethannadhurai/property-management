cdebfaba404d0d5e59c20762f57615d8531d6255
sweth <swethaannadhurai@gmail.com>
2024-06-17 21:37:41 +0530

    Implemented user registration functionality

--- src/main/java/com/mycompany/propertymanagement/controller/CslculatorMain.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CslculatorMain.java
@@ -5,6 +5,5 @@ public class CslculatorMain {
     public static void main(String[] args){
         calculatorController cc = new calculatorController();
         Double result=cc.add(14.5,8.5,6.1);
-        System.out.println(result);
     }
 }
